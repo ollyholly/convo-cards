ff029713c0f3df00f605ed964ad89cb9f8c64a2e
Olly Holovchenko <oleh.holo@gmail.com>
2023-03-15 00:39:55 +0000

    Add countdown and timer setup features.

--- src/pages/Timer.js
+++ src/pages/Timer.js
@@ -1,14 +1,33 @@
-import { Container, Box, Typography, Card, Button } from '@mui/material';
+import { Container, Box, Typography, Card, Button, Stack, Select, FormControl, MenuItem, InputLabel} from '@mui/material';
 import { useState, useEffect} from 'react';
 
 import { emoji } from '../assets/emoji';
 
-const TIME = 1000
+const DEFAULT_TIME_IN_SECONDS = 15
 
 const Timer = () => {
 
   const [q, setQ] = useState(0);
   const [timerOn, setTimerOn] = useState(false)
+  const [time, setTime] = useState(DEFAULT_TIME_IN_SECONDS);
+  const mins = Math.floor(time / 60)
+  const secs = Math.floor(time % 60)
+
+  const tick = () => {
+    if (mins === 0 && secs === 0)
+        {handleRandom()
+          reset()}
+          
+        setTime((time) => time -1);
+
+};
+
+const handleChange = (event) => {
+  setTime(event.target.value);
+};
+
+const reset = () => setTime(DEFAULT_TIME_IN_SECONDS);
+
  
   const handleRandom = () => {
     const random = Math.floor(Math.random() * emoji.length);
@@ -22,14 +41,13 @@ useEffect(()=>{
   if (timerOn) {
 
   const interval = setInterval(() => {
-    console.log('Logs every sec');
-    handleRandom()
-  }, TIME);
+    tick()
+  }, 1000);
 
   return () => clearInterval(interval);
   }
 
-}, [timerOn])
+})
 
   return (
     <Container>
@@ -58,10 +76,39 @@ useEffect(()=>{
          
         </Box>
         <Box sx={{ display: 'flex', justifyContent: 'center' }} marginTop={2}>
+        <Stack direction="row" spacing={2}>
           <Button 
           variant="contained" 
           color="blacky"
           onClick={handleTimer}>{timerOn ? 'STOP TIMER' : 'START TIMER'}</Button>
+          <Button 
+          variant="contained"
+          onClick={reset}>Reset</Button>
+          </Stack>
+        </Box>
+        <Box sx={{ display: 'flex', justifyContent: 'center' }} marginTop={2}>
+        <Typography variant="h4" my={5} align="center">
+        {`${mins
+            .toString()
+            .padStart(2, '0')}:${secs.toString().padStart(2, '0')}`}
+            </Typography>
+        </Box>
+        <Box sx={{ display: 'flex', justifyContent: 'center' }} marginTop={2}>
+        <FormControl sx={{ m: 1, minWidth: 120 }}>
+  <InputLabel id="demo-simple-select-label">Age</InputLabel>
+  <Select
+    labelId="demo-simple-select-label"
+    id="demo-simple-select"
+    value={time}
+    label="Time"
+    onChange={handleChange}
+  >
+    <MenuItem value={15}>15 sec</MenuItem>
+    <MenuItem value={30}>30 sec</MenuItem>
+    <MenuItem value={60}>1 min</MenuItem>
+    <MenuItem value={180}>3 min</MenuItem>
+  </Select>
+</FormControl>
         </Box>
     </Container>
   );
